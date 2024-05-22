9d643dd1adb5ad48a7a7d05ef66949a879716100
knaresh88 <162580924+knaresh88@users.noreply.github.com>
2024-05-23 02:07:56 +0530

    Update jest.config.js

--- Frontend/jest.config.js
+++ Frontend/jest.config.js
@@ -1,17 +1,16 @@
 // jest.config.js
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
   },
-  "moduleFileExtensions": [
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],  
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
