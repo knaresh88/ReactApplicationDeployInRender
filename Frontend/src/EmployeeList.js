b9b5eb49360c60109e7d63e72166f563fe82bdc8
knaresh88 <162580924+knaresh88@users.noreply.github.com>
2024-05-23 16:45:33 +0530

    Update2 EmployeeList.js

--- Frontend/src/EmployeeList.js
+++ Frontend/src/EmployeeList.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import EditEmployeeForm from './EditEmployeeForm';
+import EditEmployeeForm from '../EditEmployeeForm';
 
 const EmployeeList = () => {
   const [employeeList, setEmployees] = useState([]);
